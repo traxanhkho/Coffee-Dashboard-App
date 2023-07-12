6543878b8895bfc59c3775250e79a95cb9df97df
datdang <38239103+traxanhkho@users.noreply.github.com>
2023-07-12 10:16:43 +0700

    add layouts component

--- src/app/products/productForm/page.js
+++ src/app/products/productForm/page.js
@@ -19,6 +19,7 @@ import {
 } from "@/services/productService";
 import { getGenres } from "@/services/genreService";
 import { getToppings } from "@/services/toppingService";
+import Layouts from "@/components/Layouts";
 
 export default function productForm() {
   const router = useRouter();
@@ -107,140 +108,145 @@ export default function productForm() {
   // https://coffee-server-test.onrender.com/api/products
 
   return (
-    <Container>
-      <div className="py-4 lg:py-8 md:grid md:grid-cols-3 md:gap-6">
-        <div className="md:col-span-1">
-          <div className="px-4 sm:px-0">
-            <h3 className="text-lg font-medium leading-6 text-gray-900">
-              Thông tin sản phẩm
-            </h3>
-            <p className="mt-1 text-sm text-gray-600">
-              Mục tiêu chính của trang thông tin sản phẩm là cung cấp đầy đủ
-              thông tin về sản phẩm để khách hàng có thể đưa ra quyết định mua
-              hàng thông minh và hài lòng với sự lựa chọn của mình.
-            </p>
+    <Layouts>
+      <Container>
+        <div className="py-4 lg:py-8 md:grid md:grid-cols-3 md:gap-6">
+          <div className="md:col-span-1">
+            <div className="px-4 sm:px-0">
+              <h3 className="text-lg font-medium leading-6 text-gray-900">
+                Thông tin sản phẩm
+              </h3>
+              <p className="mt-1 text-sm text-gray-600">
+                Mục tiêu chính của trang thông tin sản phẩm là cung cấp đầy đủ
+                thông tin về sản phẩm để khách hàng có thể đưa ra quyết định mua
+                hàng thông minh và hài lòng với sự lựa chọn của mình.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="mt-5 md:col-span-2 md:mt-0">
-          <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
-            <div className="shadow sm:overflow-hidden sm:rounded-md">
-              <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
-                <Input
-                  register={register}
-                  name="name"
-                  errors={errors}
-                  label="Tên sản phẩm"
-                  placeholder={"Nhập tên sản phẩm"}
-                />
-                <Textarea
-                  register={register}
-                  label="Mô tả sản phẩm"
-                  name="description"
-                  errors={errors}
-                  rows={3}
-                  placeholder="Nhập mô tả sản phẩm"
-                  defaultValue=""
-                />
-                <SelectMenu
-                  register={register}
-                  label="Loại sản phẩm"
-                  name="genre"
-                  items={genres}
-                />
-                <Input
-                  register={register}
-                  name="price"
-                  type="number"
-                  isPriceField={true}
-                  min={0}
-                  defaultValue={0}
-                  errors={errors}
-                  label="Giá gốc sản phẩm"
-                  placeholder={"Nhập giá gốc của sản phẩm"}
-                />
-                <Input
-                  register={register}
-                  name="numberInStock"
-                  type="number"
-                  isPriceField={true}
-                  min={0}
-                  defaultValue={0}
-                  errors={errors}
-                  label="Số lượng sản phẩm"
-                  placeholder={"Nhập số lượng sản phẩm"}
-                />
-                <div className="grid grid-cols-3 gap-4">
+          <div className="mt-5 md:col-span-2 md:mt-0">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              encType="multipart/form-data"
+            >
+              <div className="shadow sm:overflow-hidden sm:rounded-md">
+                <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
                   <Input
                     register={register}
-                    name="priceSizeS"
-                    type="number"
-                    isPriceField={true}
-                    min={0}
-                    defaultValue={0}
+                    name="name"
                     errors={errors}
-                    label="Giá size nhỏ"
-                    placeholder={"Nhập giá size nhỏ"}
+                    label="Tên sản phẩm"
+                    placeholder={"Nhập tên sản phẩm"}
+                  />
+                  <Textarea
+                    register={register}
+                    label="Mô tả sản phẩm"
+                    name="description"
+                    errors={errors}
+                    rows={3}
+                    placeholder="Nhập mô tả sản phẩm"
+                    defaultValue=""
+                  />
+                  <SelectMenu
+                    register={register}
+                    label="Loại sản phẩm"
+                    name="genre"
+                    items={genres}
                   />
                   <Input
                     register={register}
-                    name="priceSizeM"
+                    name="price"
                     type="number"
                     isPriceField={true}
                     min={0}
                     defaultValue={0}
                     errors={errors}
-                    label="Giá size vừa"
-                    placeholder={"Nhập giá size vừa"}
+                    label="Giá gốc sản phẩm"
+                    placeholder={"Nhập giá gốc của sản phẩm"}
                   />
                   <Input
                     register={register}
-                    name="priceSizeXL"
+                    name="numberInStock"
                     type="number"
                     isPriceField={true}
                     min={0}
                     defaultValue={0}
                     errors={errors}
-                    label="Giá size lớn"
-                    placeholder={"Nhập giá size lớn"}
+                    label="Số lượng sản phẩm"
+                    placeholder={"Nhập số lượng sản phẩm"}
+                  />
+                  <div className="grid grid-cols-3 gap-4">
+                    <Input
+                      register={register}
+                      name="priceSizeS"
+                      type="number"
+                      isPriceField={true}
+                      min={0}
+                      defaultValue={0}
+                      errors={errors}
+                      label="Giá size nhỏ"
+                      placeholder={"Nhập giá size nhỏ"}
+                    />
+                    <Input
+                      register={register}
+                      name="priceSizeM"
+                      type="number"
+                      isPriceField={true}
+                      min={0}
+                      defaultValue={0}
+                      errors={errors}
+                      label="Giá size vừa"
+                      placeholder={"Nhập giá size vừa"}
+                    />
+                    <Input
+                      register={register}
+                      name="priceSizeXL"
+                      type="number"
+                      isPriceField={true}
+                      min={0}
+                      defaultValue={0}
+                      errors={errors}
+                      label="Giá size lớn"
+                      placeholder={"Nhập giá size lớn"}
+                    />
+                  </div>
+
+                  <Btn size="md" onClick={() => setOpenModal(!openModal)}>
+                    Thêm Toppings
+                  </Btn>
+                  <Modal
+                    open={openModal}
+                    title="Vui lòng chọn toppings"
+                    setOpen={setOpenModal}
+                  >
+                    <Checkbox
+                      register={register}
+                      toppings={toppings}
+                      selectedToppings={selectedToppings}
+                      setSelectedToppings={setSelectedToppings}
+                    />
+
+                    {/* <ToppingTag register={register} name="toppings" /> */}
+                  </Modal>
+                  <ImageProductUploader
+                    register={register}
+                    setSelectedFile={setSelectedFile}
+                    image={image}
+                    setImage={setImage}
                   />
                 </div>
-
-                <Btn size="md" onClick={() => setOpenModal(!openModal)}>
-                  Thêm Toppings
-                </Btn>
-                <Modal
-                  open={openModal}
-                  title="Vui lòng chọn toppings"
-                  setOpen={setOpenModal}
-                >
-                  <Checkbox
-                    register={register}
-                    toppings={toppings}
-                    selectedToppings={selectedToppings}
-                    setSelectedToppings={setSelectedToppings}
-                  />
-
-                  {/* <ToppingTag register={register} name="toppings" /> */}
-                </Modal>
-                <ImageProductUploader
-                  register={register}
-                  setSelectedFile={setSelectedFile}
-                  image={image}
-                  setImage={setImage}
-                />
+                <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Lưu sản phẩm
+                  </button>
+                </div>
               </div>
-              <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                <button
-                  type="submit"
-                  className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Lưu sản phẩm
-                </button>
-              </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </Layouts>
   );
 }
